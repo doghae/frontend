8523cac22d6d51b4fdc2f6d668cbd9cc9a8df598
parknew0 <psy_chopath@naver.com>
2024-08-02 09:57:31 +0900

    fix: 반응형 화면에서 최소로 축소했을 때 발생하는 버그 픽스

--- src/components/HomePage/Contents.tsx
+++ src/components/HomePage/Contents.tsx
@@ -1,3 +1,5 @@
+import styled from "@emotion/styled";
+
 import { Grid, GridItem } from "@chakra-ui/react";
 import { Section1 } from "./Section1";
 import { Section2 } from "./Section2";
@@ -7,13 +9,14 @@ import { Loginbox } from "./Loginbox";
 
 export const Contents = () => {
   return (
-    <>
+    <Wrapper>
       <Grid
-        h={{ base: "1600px", md: "2000px" }}
+        h={{ base: "2000px", md: "2500px" }}
         w="100%"
         templateRows={{ base: "repeat(4, 1fr)", md: "1fr 300px 300px 500px" }}
         templateColumns={{ base: "1fr", md: "repeat(1, 1fr)" }}
         gap={{ base: 50, md: 100 }}
+        p={{ base: 5, md: 8 }}  /* 화면 크기에 따른 패딩 설정 */
       >
         <GridItem id="section1" rowSpan={1} backgroundColor={"transparent"}>
           <Section1 />
@@ -27,9 +30,14 @@ export const Contents = () => {
         <GridItem id="section4" rowSpan={1} backgroundColor={"transparent"}>
           <Section4 />
         </GridItem>
+        <GridItem id="Loginbox" rowSpan={1} backgroundColor={"transparent"}>
+          <Loginbox />
+        </GridItem>
       </Grid>
-
-      <Loginbox />
-    </>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  overflow: hidden; /* 전체 스크롤 숨김 */
+`;

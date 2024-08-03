e121f20e812d57f35828caacfccf6dba76fb91be
kiyeong <kjr07145@gmail.com>
2024-08-03 23:19:48 +0900

    chore: post URL 변경

--- src/pages/KakaoRedirectHandler.tsx
+++ src/pages/KakaoRedirectHandler.tsx
@@ -1,14 +1,14 @@
 // src/pages/KakaoRedirectHandler.tsx
 
-import React, { useEffect } from 'react';
-import axios from 'axios';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
 
 const KakaoRedirectHandler = () => {
   const location = useLocation();
 
   useEffect(() => {
-    const authorizationCode = new URLSearchParams(location.search).get('code');
+    const authorizationCode = new URLSearchParams(location.search).get("code");
     if (authorizationCode) {
       getToken(authorizationCode);
     }
@@ -16,23 +16,26 @@ const KakaoRedirectHandler = () => {
 
   const getToken = async (authorizationCode: string) => {
     const REST_API_KEY = "af5896ef6b5436cd1b8d653c769c823e";
-    const REDIRECT_URI = "http://3.34.86.29:8080/oauth2/kakao/login";
-    
+    const REDIRECT_URI = "http://3.34.86.29:8080/oauth2/kakao/login?";
+
     try {
-      const response = await axios.post('http://3.34.86.29:8080/oauth2/kakao/token', {
-        code: authorizationCode,
-        client_id: REST_API_KEY,
-        redirect_uri: REDIRECT_URI,
-        grant_type: 'authorization_code',
-      });
+      const response = await axios.post(
+        "http://3.34.86.29:8080/oauth2/kakao/login?code=HVP-kH6HBWQrW9MXoQHP2F5xydyVyWEHsp0vLqYjFP8vMQvAvVoCLgAAAAQKKcjYAAABkRiVAe_OkqTnJF629A",
+        {
+          code: authorizationCode,
+          client_id: REST_API_KEY,
+          redirect_uri: REDIRECT_URI,
+          grant_type: "authorization_code",
+        }
+      );
 
       const { access_token } = response.data;
       // 로컬 스토리지에 토큰 저장
-      localStorage.setItem('token', access_token);
+      localStorage.setItem("token", access_token);
 
       // 토큰을 이용한 사용자 정보 요청 등 필요한 작업
     } catch (error) {
-      console.error('토큰 요청 에러:', error);
+      console.error("토큰 요청 에러:", error);
     }
   };
 
